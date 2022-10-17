a810cbbbb50ba471acb52578c3edaaa0800863af
Gloria <gloghe@yahoo.it>
2022-10-17 16:09:25 +0200

    stile register e login

--- login.php
+++ login.php
@@ -1,31 +1,31 @@
 <?php include('server.php') ?>
 <!DOCTYPE html>
 <html>
+
 <head>
-  <title>Edusogno</title>
-  <link rel="stylesheet" type="text/css" href="assets/styles/style.css">
+	<title>Edusogno</title>
+	<link rel="stylesheet" type="text/css" href="assets/styles/style.css">
 </head>
+
 <body>
-  <div class="header">
-  	<h2>Login</h2>
-  </div>
-	 
-  <form method="post" action="login.php">
-  	<?php include('errors.php'); ?>
-  	<div class="input-group">
-  		<label>Inserisci l'e-mail</label>
-  		<input type="text" name="email" >
-  	</div>
-  	<div class="input-group">
-  		<label>Inserisci la password</label>
-  		<input type="password" name="password">
-  	</div>
-  	<div class="input-group">
-  		<button type="submit" class="btn" name="login_user">ACCEDI</button>
-  	</div>
-  	<p>
-  		Non hai ancora un profilo? <a href="register.php">Registrati</a>
-  	</p>
-  </form>
+	<section id="rectangle2">
+		<div id="rectangle1">
+			<h1>Crea il tuo account</h1>
+			<form method="post" action="login.php" id="formLogin">
+				<?php include('errors.php'); ?>
+					<label class="label" id="Label1">Inserisci l'e-mail</label>
+					<input class="input" id="Input1" type="text" name="email">
+					<span class="line" id="line1"></span>
+					<label class="label" id="Label2">Inserisci la password</label>
+					<input class="input" id="Input2" type="password" name="password">
+					<span class="line" id="line2"></span>
+					<button type="submit" class="button" name="login_user" id="buttonLogin">ACCEDI</button>
+				<p>
+					Non hai ancora un profilo? <a href="register.php">Registrati</a>
+				</p>
+			</form>
+		</div>
+	</section>
 </body>
+
 </html>
--- register.php
+++ register.php
@@ -1,33 +1,42 @@
 <?php include('server.php') ?>
 <!DOCTYPE html>
 <html>
+
 <head>
-  <title>Edusogno</title>
-  <link rel="stylesheet" type="text/css" href="assets/styles/style.css">
+	<title>Edusogno</title>
+	<link rel="stylesheet" type="text/css" href="assets/styles/style.css">
 </head>
+
 <body>
 	<section id="rectangle2">
-        <div id="rectangle1">
-            <h1>Crea il tuo account</h1>
-				
-			<form method="post" action="register.php"  id="rectangle9">
+		<div id="rectangle1">
+			<h1>Crea il tuo account</h1>
+
+			<form method="post" action="register.php" id="formRegistration">
 				<?php include('errors.php'); ?>
-				<label class="label" id="registrationLabelName" for="nome">Inserisci il nome</label>
-					<input class="input" id="registrationInputName" type="text" name="nome" id="nome" value="<?php echo $nome; ?>">
-					<span class="line" id="line1"></span>
-					<label class="label" id="registrationLabelCognome" for="cognome">Inserisci il cognome</label>
-					<input class="input" id="registrationInputCognome" type="text" name="cognome" id="cognome" value="<?php echo $cognome; ?>">
-					<span class="line" id="line2"></span>
-					<label class="label" id="registrationLabelEmail" for="email">Inserisci l'email</label>
-					<input class="input" id="registrationInputEmail" type="email" name="email" id="email"  value="<?php echo $email; ?>">
-					<span class="line" id="line3"></span>
-					<label class="label" id="registrationLabelPassword" for="password">Inserisci la password</label>
-					<input class="input" id="registrationInputPassword" type="text" name="password" id="password">
-					<span class="line" id="line4"></span>
-					<button type="submit" class="btn" name="reg_user" id="buttonRegistration">REGISTRATI</button>
-  			</form>
-  		</div>
-    </section>
-  
+				<label class="label" id="Label1" for="nome">Inserisci il nome</label>
+				<input class="input" id="Input1" type="text" name="nome" id="nome"
+					value="<?php echo $nome; ?>">
+				<span class="line" id="line1"></span>
+				<label class="label" id="Label2" for="cognome">Inserisci il cognome</label>
+				<input class="input" id="Input2" type="text" name="cognome" id="cognome"
+					value="<?php echo $cognome; ?>">
+				<span class="line" id="line2"></span>
+				<label class="label" id="Label3" for="email">Inserisci l'email</label>
+				<input class="input" id="Input3" type="email" name="email" id="email"
+					value="<?php echo $email; ?>">
+				<span class="line" id="line3"></span>
+				<label class="label" id="Label4" for="password">Inserisci la password</label>
+				<input class="input" id="Input4" type="text" name="password" id="password">
+				<span class="line" id="line4"></span>
+				<button type="submit" class="button" name="reg_user" id="buttonRegistration">REGISTRATI</button>
+				<p>
+					Hai già un account? <a href="login.php">Accedi</a>
+				</p>
+			</form>
+		</div>
+	</section>
+
 </body>
+
 </html>
